e99c217f988a08e357179fe24adb8d5f36b37c00
Andy Green <aj.green07@outlook.com>
2021-12-08 22:01:07 +0000

    javascript practice

--- pigLatin.js
+++ pigLatin.js
@@ -1,15 +1,33 @@
 /* Challenge:
-    Write a function that will convert a word to Pig Latin
+    Write a function that will convert a word to Pig Latin.
     If a word begins with a consonant, take the first consonant or consonant cluster and move it to the end of the word. Once that’s done, add an “ay” at the end.
     If the word doesn’t have a vowel, just add an “ay” at the end.
     If a word begins with a vowel, just add “way” at the end.
 */
-function translateToPigLatin(str) {
+function transWordToPigLatin(str) {
+    // initial values of vowels and the final string to be returned
     let vowels = ['a', 'e', 'i', 'o', 'u'];
-    let newStr = "";
+    let newStr = '';
+
     // if word begins with a vowel
-    if(vowels.indexOf(str[0]) > -1);
-        newStr = str + "way";
+    if (vowels.indexOf(str[0]) > -1) {
+        newStr = str + 'way';
+        return newStr;
+    } else {
+        // check if word contains a vowel. If not 'firstMatch' is set to '0'
+        let firstMatch = str.match(/[aeiou]/g) || 0;
+
+        // set 'vowel' to the index of the first vowel found in the string
+        let vowel = str.indexOf(firstMatch[0]);
+
+        // everything from the first vowel + starting consonants + ay
+        newStr = str.substring(vowel) + str.substring(0, vowel) + 'ay';
+        return newStr;
+    }
 };
 
-translateToPigLatin("i am here");
+console.log(transWordToPigLatin("california"));
+console.log(transWordToPigLatin("glove"));
+console.log(transWordToPigLatin("rhythm"));
+console.log(transWordToPigLatin("eight"));
+console.log(transWordToPigLatin("sky"));
--- vowels.js
+++ vowels.js
@@ -2,6 +2,7 @@
     Given a string, return a new string where 'av' is added before each vowel
 */
 const translate = (str) => {
+    // set array of vowels
     const vowels = ['a', 'e', 'i', 'o', 'u'];
     let trans = str.split("");
     for (i=0; i<trans.length; i++) {
